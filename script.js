3e1bf701eff5f64e706de8e66e85f9529ab5dca9
2nav <n-raj@outlook.com>
2022-03-18 02:41:37 +0530

    Added 30 day chart (v1.0.1)

--- script.js
+++ script.js
@@ -163,3 +163,58 @@ vats.open("GET", "https://api.covid19api.com/dayone/country/india", false);
 vats.send();
 
 let parsedvats = JSON.parse(vats.responseText)
+
+function Covid5(parsedResponse){
+    function genlabel()
+    {
+        let leng = parsedResponse.length;
+        let datarr = []
+        for (let i = leng - 30; i < leng; i++) {
+            datarr.push(parsedResponse[i].Date.substring(0,10))
+        }
+        return datarr
+    }
+    const labels = genlabel();
+
+    function gendata(){
+        let leng = parsedResponse.length;
+        let datarr = []
+        for (let i = leng - 30; i < leng; i++) {
+            datarr.push(parsedResponse[i].Confirmed - parsedResponse[i - 1].Confirmed)
+        }
+        return datarr;
+    }
+    
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'Daily COVID cases in India',
+        backgroundColor: 'rgb(254, 139, 139)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: gendata(),
+      }]
+    };
+  
+    const config = {
+      type: 'bar',
+      data: data,
+      options: {
+          plugins: {
+              legend: {
+                  display: true,
+                  labels: {
+                      color: 'rgb(255, 99, 132)'
+                  }
+              }
+          }
+      }
+    };
+    const myChart = new Chart(
+        document.getElementById('ch5'),
+      config
+    );
+  }
+
+  
+  
+Covid5(parsedvats);
